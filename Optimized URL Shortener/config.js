88df0185ab0e8a56bf652a14c801fbc22f325682
MohammedNoorAhmed <mohammednoorahmed05@gmail.com>
2024-06-10 15:33:16 +0530

    Update config.js

--- Optimized URL Shortener/config.js
+++ Optimized URL Shortener/config.js
@@ -1,12 +1,12 @@
 // config.js
 const config = {
     virusTotal: {
-      apiKey: "e32d706ddc76727559f6891f49fd915010029cb662eb110a9a1cfc0431a3b568",
+      apiKey: "write your api key here",
       postUrl: "https://www.virustotal.com/api/v3/urls",
       getUrl: (id) => `https://www.virustotal.com/api/v3/analyses/${id}`
     },
     bitly: {
-      apiKey: "c46d9bc50d7d73df3acce33edbe612fd6f305f31",
+      apiKey: "write your api key here",
       shortenUrl: "https://api-ssl.bitly.com/v4/shorten"
     }
   };
